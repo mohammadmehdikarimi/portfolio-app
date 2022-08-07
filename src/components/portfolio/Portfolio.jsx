54a603b5aa2e5d012d10ddc634c50650ea1c642b
mohammadmehdikarimi <karimimohammadmehdi99@gmail.com>
2022-08-07 04:49:48 +0000

    portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,118 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG31 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
-  )
-}
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-export default Portfolio
+      <div className="containert portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is portfolio item title</h3>
+          <a href="http://github.com" className="btn">
+            Github
+          </a>
+          <a
+            href="http://https://dribbble.com/"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is portfolio item title</h3>
+          <a href="http://github.com" className="btn">
+            Github
+          </a>
+          <a
+            href="http://https://dribbble.com/"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is portfolio item title</h3>
+          <a href="http://github.com" className="btn">
+            Github
+          </a>
+          <a
+            href="http://https://dribbble.com/"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is portfolio item title</h3>
+          <a href="http://github.com" className="btn">
+            Github
+          </a>
+          <a
+            href="http://https://dribbble.com/"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is portfolio item title</h3>
+          <a href="http://github.com" className="btn">
+            Github
+          </a>
+          <a
+            href="http://https://dribbble.com/"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is portfolio item title</h3>
+          <a href="http://github.com" className="btn">
+            Github
+          </a>
+          <a
+            href="http://https://dribbble.com/"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
