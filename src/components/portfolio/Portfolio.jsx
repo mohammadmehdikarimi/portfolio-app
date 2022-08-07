710ac4255357d0864098fa503139fbe6d4f7a94e
mohammadmehdikarimi <karimimohammadmehdi99@gmail.com>
2022-08-07 05:01:44 +0000

    add a new class for buttons

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -13,102 +13,114 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className="containert portfolio__container">
+      <div className="container portfolio__container">
         <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={IMG1} alt="" />
           </div>
           <h3>This is portfolio item title</h3>
-          <a href="http://github.com" className="btn">
-            Github
-          </a>
-          <a
-            href="http://https://dribbble.com/"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
+          <div className="portfolio__item-cta">
+            <a href="http://github.com" className="btn">
+              Github
+            </a>
+            <a
+              href="http://https://dribbble.com/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={IMG1} alt="" />
           </div>
           <h3>This is portfolio item title</h3>
-          <a href="http://github.com" className="btn">
-            Github
-          </a>
-          <a
-            href="http://https://dribbble.com/"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
+          <div className="portfolio__item-cta">
+            <a href="http://github.com" className="btn">
+              Github
+            </a>
+            <a
+              href="http://https://dribbble.com/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={IMG1} alt="" />
           </div>
           <h3>This is portfolio item title</h3>
-          <a href="http://github.com" className="btn">
-            Github
-          </a>
-          <a
-            href="http://https://dribbble.com/"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
+          <div className="portfolio__item-cta">
+            <a href="http://github.com" className="btn">
+              Github
+            </a>
+            <a
+              href="http://https://dribbble.com/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={IMG1} alt="" />
           </div>
           <h3>This is portfolio item title</h3>
-          <a href="http://github.com" className="btn">
-            Github
-          </a>
-          <a
-            href="http://https://dribbble.com/"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
+          <div className="portfolio__item-cta">
+            <a href="http://github.com" className="btn">
+              Github
+            </a>
+            <a
+              href="http://https://dribbble.com/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={IMG1} alt="" />
           </div>
           <h3>This is portfolio item title</h3>
-          <a href="http://github.com" className="btn">
-            Github
-          </a>
-          <a
-            href="http://https://dribbble.com/"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
+          <div className="portfolio__item-cta">
+            <a href="http://github.com" className="btn">
+              Github
+            </a>
+            <a
+              href="http://https://dribbble.com/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={IMG1} alt="" />
           </div>
           <h3>This is portfolio item title</h3>
-          <a href="http://github.com" className="btn">
-            Github
-          </a>
-          <a
-            href="http://https://dribbble.com/"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
+          <div className="portfolio__item-cta">
+            <a href="http://github.com" className="btn">
+              Github
+            </a>
+            <a
+              href="http://https://dribbble.com/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
       </div>
     </section>
